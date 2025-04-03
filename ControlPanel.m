1c0fb91db375845858e2d5f13f78e87a2e2ceb52
Jillian To <jtoschool388@gmail.com>
2025-04-03 12:37:45 -0400

    added some accessors and setters

--- ControlPanel.m
+++ ControlPanel.m
@@ -1,5 +1,5 @@
 classdef ControlPanel
-    properties
+    properties (Access = private)
         % Array of 3 integer coordinates: X, Y, Z
         coords
         % Integer that represents mode: 
@@ -21,6 +21,28 @@ classdef ControlPanel
                 obj.coords = init_coords
                 obj.mode = init_mode
             end
+        end
+        function setCoords(obj, new_coords)
+            obj.coords = new_coords
+        end
+        function curr_coords = getCoords(obj)
+            switch obj.mode
+                % Stand-by 
+                case 0
+                    % TODO: squitter capability, set small random probability of sending coords,
+                    % else don't send coords 
+                    % TODO: respond to discrete interrogation capability
+                    curr_coords = []
+                % Mode S transponder is fully operational in other modes
+                otherwise
+                    curr_coords = obj.coords
+            end
+        end
+        function setMode(obj, new_mode)
+            obj.mode = new_mode
+        end
+        function curr_mode = getMode(obj)
+            curr_mode = obj.mode
         end
     end
 end
